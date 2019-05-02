e1e8086f2de97193b6b0411da44851140803e58c
Matteo Corti <matteo@corti.li>
2019-05-02 15:21:38 +0200

    Catches errors from tmutil lastbackup

--- tmstatus.sh
+++ tmstatus.sh
@@ -83,9 +83,15 @@ else
     backup_date=$( tmutil listbackups  | head -n 1 | sed 's/.*\///' | sed 's/-\([^\-]*\)$/\ \1/' | sed 's/\([0-9][0-9]\)\([0-9][0-9]\)\([0-9][0-9]\)/\1:\2:\3/')
     printf 'Oldest:\t\t%s (%s)\n' "${backup_date}" "$( format_days_ago "${days}" )"
 
-    days=$( days_since "$( tmutil latestbackup | sed 's/.*\///' )" )
-    backup_date=$( tmutil latestbackup | sed 's/.*\///' | sed 's/-\([^\-]*\)$/\ \1/' | sed 's/\([0-9][0-9]\)\([0-9][0-9]\)\([0-9][0-9]\)/\1:\2:\3/')
-    printf 'Last:\t\t%s (%s)\n' "${backup_date}" "$( format_days_ago "${days}" )"
+    latestbackup="$( tmutil latestbackup )"
+    if echo "${lastestbackup}" | grep -q '[0-9]' ; then
+	# a date was returned
+	days=$( days_since "$( tmutil latestbackup | sed 's/.*\///' )" )
+	backup_date=$( tmutil latestbackup | sed 's/.*\///' | sed 's/-\([^\-]*\)$/\ \1/' | sed 's/\([0-9][0-9]\)\([0-9][0-9]\)\([0-9][0-9]\)/\1:\2:\3/')
+	printf 'Last:\t\t%s (%s)\n' "${backup_date}" "$( format_days_ago "${days}" )"
+    else
+	printf 'Last:\t\t%s\n' "${lastbackup}"
+    fi
 
     number=$( tmutil listbackups | wc -l | sed 's/\ //g' )
     printf 'Number:\t\t%s\n' "${number}"
