0793a45f7b1f6cf5b0a180a254b00c15c574ef0b
Matteo Corti <matteo@corti.li>
2021-10-26 15:05:33 +0200

    Fixing also latestbackuop

--- tmstatus.sh
+++ tmstatus.sh
@@ -121,8 +121,8 @@ else
     latestbackup="$(tmutil latestbackup)"
     if echo "${latestbackup}" | grep -q '[0-9]'; then
         # a date was returned (should implement a better test)
-        days=$(days_since "$(tmutil latestbackup | sed 's/.*\///')")
-        backup_date=$(tmutil latestbackup | sed 's/.*\///' | sed 's/-\([^\-]*\)$/\ \1/' | sed 's/\([0-9][0-9]\)\([0-9][0-9]\)\([0-9][0-9]\)/\1:\2:\3/')
+        days=$(days_since "$(tmutil latestbackup | sed 's/.*\///' | sed 's/[.].*//' )")
+        backup_date=$(tmutil latestbackup | sed 's/.*\///' | sed 's/[.].*//' | sed 's/-\([^\-]*\)$/\ \1/' | sed 's/\([0-9][0-9]\)\([0-9][0-9]\)\([0-9][0-9]\)/\1:\2:\3/')
         printf 'Last:\t\t%s (%s)\n' "${backup_date}" "$(format_days_ago "${days}")"
     else
         printf 'Last:\t\t%s\n' "${latestbackup}"
